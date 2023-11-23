cdd789d2a9a3cf2219af7190a2fd6e2b8c4342ee
☄️ MD. Jahid Hasan <mjahidhasand4@gmail.com>
2023-11-23 07:45:31 +0600

    🪹 Created the Doubly Linked List
    Created Methods are:
    🪺 Push
    🪺 Pop
    🪺 Unshift
    🪺 Shift
    🪺 Clear
    🪺 Print
    
    🗃️ Changed the folder structure

--- typescript/list.ts
+++ typescript/list.ts
@@ -1,3 +1,5 @@
+/** Singly Linked List */
+
 class SinglyNode<T> {
   public data: T;
   public next: SinglyNode<T> | null;
@@ -8,7 +10,7 @@ class SinglyNode<T> {
   }
 }
 
-class SinglyList<T> {
+class Singly<T> {
   private head: SinglyNode<T> | null;
   public values: T[];
 
@@ -89,6 +91,138 @@ class SinglyList<T> {
     while (current !== null) {
       data.push(current.data);
       current = current.next;
+    }
+
+    console.log(data);
+  };
+}
+
+/** Doubly Linked List */
+
+class DoublyNode<T> {
+  public data: T;
+  public previous: DoublyNode<T> | null;
+  public next: DoublyNode<T> | null;
+
+  constructor(data: T) {
+    this.data = data;
+    this.previous = null;
+    this.next = null;
+  }
+}
+
+class Doubly<T> {
+  private head: DoublyNode<T> | null;
+  private tail: DoublyNode<T> | null;
+  public values: T[];
+  public length: number;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.values = [];
+    this.length = 0;
+  }
+
+  public push = (data: T): void => {
+    const newNode = new DoublyNode(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      this.length += 1;
+      this.values.push(data);
+    } else {
+      newNode.previous = this.tail;
+      this.tail!.next = newNode;
+      this.tail = newNode;
+
+      this.length += 1;
+      this.values.push(data);
+    }
+  };
+
+  public pop = () => {
+    if (this.head === null) return undefined;
+
+    const data = this.tail?.data;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail!.previous;
+      this.tail!.next = null;
+    }
+
+    this.values.pop();
+    this.length -= 1;
+
+    return data;
+  };
+
+  public unshift = (data: T) => {
+    const newNode = new DoublyNode(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      this.length += 1;
+      this.values.unshift(data);
+    } else {
+      newNode.next = this.head;
+      this.head.previous = newNode;
+      this.head = newNode;
+
+      this.length += 1;
+      this.values.unshift(data);
+    }
+  };
+
+  public shift = () => {
+    if (this.head === null) return undefined;
+
+    const data = this.head.data;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head!.previous = null;
+    }
+
+    this.values.shift();
+    this.length -= 1;
+
+    return data;
+  };
+
+  public clear = () => {
+    this.head = null;
+    this.tail = null;
+
+    this.values = [];
+    this.length = 0;
+  };
+
+  public print = (reverse = false) => {
+    const data: T[] = [];
+
+    if (reverse) {
+      let current = this.tail;
+      while (current) {
+        data.push(current!.data);
+        current = current!.previous;
+      }
+    } else {
+      let current = this.head;
+      while (current) {
+        data.push(current!.data);
+        current = current!.next;
+      }
     }
 
     console.log(data);
