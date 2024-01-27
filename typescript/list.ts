94ba06caea64a3cb9ece3bd68c3a41fbe7256dad
mjahidhasand4 <mjahidhasand4@gmail.com>
2024-01-27 06:43:15 +0600

    🚀 Doubly Linked List • TypeScript

--- typescript/list.ts
+++ typescript/list.ts
@@ -84,3 +84,99 @@ class Singly<T> {
     this.head = null;
   };
 }
+
+class DoublyNode<T> {
+  public data: T;
+  public previous: DoublyNode<T> | null;
+  public next: DoublyNode<T> | null;
+
+  constructor(data: T) {
+    this.data = data;
+    this.previous = null;
+    this.next = null;
+  }
+}
+
+class Doubly<T> {
+  private head: DoublyNode<T> | null;
+  private tail: DoublyNode<T> | null;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  push = (data: T): void => {
+    const newNode = new DoublyNode<T>(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+
+    this.tail!.next = newNode;
+    this.tail = newNode;
+  };
+
+  pop = (): T | undefined => {
+    if (this.head === null) return undefined;
+
+    const data = this.tail?.data;
+    this.tail = this.tail!.previous;
+
+    if (this.tail) {
+      this.tail.next = null;
+    } else {
+      this.head = null;
+    }
+
+    return data;
+  };
+
+  unshift = (data: T): void => {
+    const newNode = new DoublyNode<T>(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+
+    newNode.next = this.head;
+    this.head.previous = newNode;
+    this.head = newNode;
+  };
+
+  shift = (): T | undefined => {
+    if (this.head === null) return undefined;
+
+    const data = this.head.data;
+    this.head = this.head.next;
+
+    if (this.head) {
+      this.head.previous = null;
+    } else {
+      this.tail = null;
+    }
+
+    return data;
+  };
+
+  length = (): number => {
+    let length = 0;
+    let current = this.head;
+
+    while (current !== null) {
+      length++;
+      current = current.next;
+    }
+
+    return length;
+  };
+
+  clear = () => {
+    this.head = null;
+    this.tail = null;
+  };
+}
